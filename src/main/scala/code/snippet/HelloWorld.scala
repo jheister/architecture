ddf5882034a610008af71488d717df204a4e3476
Justin Heister <justin.heister@youdevise.com>
2013-03-25 17:20:58 +0000

    basic client side actor

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -7,17 +7,35 @@ import net.liftweb.common._
 import java.util.Date
 import code.lib._
 import Helpers._
+import net.liftweb.http.{ScopedLiftActor, S}
+import net.liftweb.actor.LiftActor
+import net.liftweb.json.JsonAST.{JValue, JString}
+import net.liftweb.http.js.JsCmds.Script
+import net.liftweb.http.js.JE.JsRaw
+import util.parsing.json.JSONFormat
+import net.liftweb.json.{DefaultFormats, Formats}
 
-class HelloWorld {
+class HelloWorld extends Logger {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
-  // replace the contents of the element with id "time" with the date
-  def howdy = "#time *" #> date.map(_.toString)
+  def howdy = (for {
+    sess <- S.session
+  } yield {
+    val ui = sess.serverActorForClient("receive")
+    val backEnd = new ScopedLiftActor {
+      override def lowPriority = {
+        case Foo(bar) => {ui ! Foo("Hello, I am a foo")}
+      }
+    }
 
-  /*
-   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+    Script(JsRaw("var sendMessage = " + sess.clientActorFor(backEnd, (value: JValue) => {
+      implicit val format = DefaultFormats
 
-   def howdy = "#time *" #> date.toString
-   */
+      value.extractOpt[Foo].map(Full(_)).getOrElse(Empty)
+    }).toJsCmd).cmd &
+           JsRaw("function receive(value) { alert(value.bar); }").cmd)
+  }) openOr NodeSeq.Empty
 }
+
+case class Foo(bar: String)
 
