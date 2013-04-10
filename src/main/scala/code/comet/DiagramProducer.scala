b8198f4df06cd377be4c6a682e977896c58d817a
Justin Heister <justin.heister@youdevise.com>
2013-04-10 11:15:54 +0100

    Revert "started converting to client side actor"
    This reverts commit 6dfe1ccb006d4e1eddcb2027470836052c254627.

--- src/main/scala/code/comet/DiagramProducer.scala
+++ src/main/scala/code/comet/DiagramProducer.scala
@@ -1,25 +1,21 @@
 package code.comet
 
 import net.liftweb.http._
-import js.JsCmds.{Script, Run}
+import js.JsCmds.Run
 import net.liftweb.util.{Helpers}
 import Helpers._
 import util.Random
-import js.{JsExp, JsCmds}
-import net.liftweb.common.{Full, Box, Logger}
+import net.liftweb.http.js.JsCmds
+import net.liftweb.common.Logger
 import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.json.JsonAST.JValue
+import net.liftweb.json.{DefaultFormats}
 import scala.Some
-import net.liftweb.actor.LiftActor
-import net.liftweb.json
-import json.{DefaultFormats, NoTypeHints}
 
 class DiagramProducer extends CometActor with CometListener with Logger {
   val graph = Area(Coordinates(0,0), Coordinates(1600, 800))
   val nodes: scala.collection.mutable.Map[String, Cell] = scala.collection.mutable.Map()
   val edges: scala.collection.mutable.Map[String, Edge] = scala.collection.mutable.Map()
-
-  val theGraph = new Graph(1600, 800, nodes.values.toSeq, edges.values.toSeq)
 
   protected def registerWith = LogEventServer
 
@@ -29,7 +25,7 @@ class DiagramProducer extends CometActor with CometListener with Logger {
     case AddNode(name) => {
       if (nodes.get(name).isEmpty) {
         val cell = Cell(name, DiagramProducer.freeCoordinates(nodes.values, graph.quarter))
-        theGraph.addCell(cell)
+        partialUpdate(cell.add)
         nodes.put(name, cell)
       }
     }
@@ -38,12 +34,12 @@ class DiagramProducer extends CometActor with CometListener with Logger {
       val option = edges.get(from + to)
       if (option.isEmpty) {
         val edge = Edge(from, to, weight)
-        theGraph.addEdge(edge)
+        partialUpdate(edge.add)
         edges.put(from + to, edge)
       } else {
         val edge = Edge(from, to, weight)
         if (option.get != edge) {
-          theGraph.updateEdge(edge)
+          partialUpdate(edge.update)
           edges.put(from + to, edge)
         }
       }
@@ -61,18 +57,10 @@ class DiagramProducer extends CometActor with CometListener with Logger {
     JsCmds.Noop
   }).toJsCmd
 
-  def render = {
-    info("XXXXXXXXXXXXXXXXXXXXXX: Rendering!")
-    nodes.values.foreach(theGraph.addCell)
-    ".graph" #> theGraph.render
-  }
+  def render = ".init *" #> Run("var graph = new Graphing.Graph(function(val) { %s }, %s, %s); graph.render();".format(cmd, graph.end.x, graph.end.y)) &
+               ".nodes *" #> JsCmds.seqJsToJs(nodes.values.map(_.add).toSeq) &
+               ".edges *" #> JsCmds.seqJsToJs(edges.values.map(_.add).toSeq)
 }
-
-case class AddCell(newCell: Cell)
-
-case class AddArrow(newArrow: Edge)
-
-case class UpdateArrow(updatedArrow: Edge)
 
 object DiagramProducer {
   def freeCoordinates(nodes: Iterable[Cell], areas: List[Area]): Coordinates = {
@@ -83,44 +71,6 @@ object DiagramProducer {
     } else {
       freeCoordinates(nodes, area.quarter)
     }
-  }
-}
-
-class Graph(width: Int, height: Int, cells: Seq[Cell], arrows: Seq[Edge]) {
-  val graph = Area(Coordinates(0,0), Coordinates(width, height))
-
-  val name = Helpers.nextFuncName
-
-  val backEnd: LiftActor = new ScopedLiftActor {}
-
-  val ui: Box[LiftActor] = S.session.map(_.serverActorForClient(name + ".processMessage"))
-
-  val client: Box[JsExp] = S.session.map(_.clientActorFor(backEnd))
-
-  def render = {
-    import json._
-    implicit val formats = Serialization.formats(NoTypeHints)
-
-    client match {
-      case Full(backEndProxy) => Script(Run("var " + name + " = new Graphing.Graph(%s, %s, %s)".format(backEndProxy.toJsCmd, graph.end.x, graph.end.y)) &
-                                        Run(name + ".render()") &
-                                        cells.map(cell => {
-                                          Run(name + ".processMessage(%s)".formatted(Serialization.write(cell)))
-                                        }))
-      case _ => <span>Failed to initialize</span>
-    }
-  }
-
-  def addCell(cell: Cell) = {
-    ui.foreach(_ ! AddCell(cell))
-  }
-
-  def addEdge(edge: Edge) = {
-    ui.foreach(_ ! AddArrow(edge))
-  }
-
-  def updateEdge(edge: Edge) = {
-    ui.foreach(_ ! UpdateArrow(edge))
   }
 }
 
